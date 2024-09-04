1bb363982c822cd8668c254f82ad31de0fb39774
User <User@DESKTOP-UAB5NA8>
2024-09-04 19:56:53 +0400

    qwer

---
+++ src/test_push/program.java
@@ -0,0 +1,17 @@
+package test_push;
+
+import java.util.Scanner;
+
+
+public class program {
+	public static void main(String args[]) {
+		System.out.println("asdasdas");
+		int a = (new Scanner(System.in)).nextInt();
+		System.out.println(a);
+		System.out.printf("new number = %.3f \n",(a+2.123456789f));
+		String s = Integer.toString(a);
+		System.out.println(s);
+		
+	}
+
+}
